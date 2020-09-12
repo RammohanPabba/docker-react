04e3f9904d2dcae906db6a374b4ef8fd43d46e6a
RammohanPabba <rammohan.pabba@gmail.com>
2020-09-12 17:04:24 +1000

    Testing Integration with AWS

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing Integration with AWS EBS
         </a>
       </header>
     </div>
